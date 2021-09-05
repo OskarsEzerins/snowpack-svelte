d0f52435a23f93fc0a36d0f28484ef4a48dfe3ed
Oskars Ezerins <oskarsezerins@users.noreply.github.com>
2021-09-05 14:08:02 +0300

    Build: add Svelte

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
+    public: '/',
+    c: '/dist',
   },
-  plugins: [
-    /* ... */
-  ],
+  plugins: ['@snowpack/plugin-svelte'],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
@@ -23,4 +22,4 @@ export default {
   buildOptions: {
     /* ... */
   },
-};
+}
